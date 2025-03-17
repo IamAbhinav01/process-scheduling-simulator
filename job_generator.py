e2fe80866547e9bbb6614e20f6207b27a7dec2f8
Abhinav <abhinavsunil@hotmail.com>
2025-03-17 21:59:40 +0530

    Implemented job generation with configurable parameters

--- job_generator.py
+++ job_generator.py
@@ -1,4 +1,3 @@
-# job_generator for generating random jobs for simulation
 import random
 
 class Job:
@@ -9,20 +8,18 @@ class Job:
         self.bursts = bursts
         self.current_burst_idx = 0
         self.progress = 0
-        self.total_progress = 0
+        self.total_progress = sum(bursts)
+        self.state = "new"  # Added a new attribute to track job state
 
     def __str__(self):
-        return f"Process[{self.pid}]"
+        return f"Process[{self.pid}]"  # Added string representation for logging
 
 def generate_jobs(num_jobs, arrival_range, priority_range, burst_range, num_bursts):
     jobs = []
-    for pid in range(num_jobs):
-        arrival_time = random.randint(arrival_range[0], arrival_range[1])
+    for i in range(num_jobs):
+        arrival = random.randint(arrival_range[0], arrival_range[1])
         priority = random.randint(priority_range[0], priority_range[1])
-        bursts = []
-        for _ in range(num_bursts):
-            bursts.append(random.randint(burst_range[0], burst_range[1]))
-            if _ < num_bursts - 1:
-                bursts.append(random.randint(burst_range[0], burst_range[1]))
-        jobs.append(Job(pid, arrival_time, priority, bursts))
+        bursts = [random.randint(burst_range[0], burst_range[1]) for _ in range(num_bursts * 2 - 1)]
+        jobs.append(Job(i, arrival, priority, bursts))
+    jobs.sort(key=lambda x: x.arrival_time)  # Added sorting by arrival time
     return jobs
